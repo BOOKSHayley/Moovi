ff88d53bc741d1c4cc3fe557766d9f561a367239
Karley <karley.waguespack@gmail.com>
2021-10-14 18:31:52 -0500

    added onLikeClicked and onDislikeClicked methods

--- lib/likeDislikeBackend.dart
+++ lib/likeDislikeBackend.dart
@@ -1,2 +1,16 @@
-import 'dart:io';
+import 'dart:core';
+import 'package:moovi/Movie.dart';
 
+var likedMovies = <Movie>[];
+var dislikedMovies = <Movie>[];
+
+void onLikeClicked(Movie movie){
+  likedMovies.add(movie);
+  //todo remove movie from Chris's queue
+
+}
+
+void onDislikeClicked(Movie movie){
+  dislikedMovies.add(movie);
+  //todo add movie to the end of Chris's queue
+}
