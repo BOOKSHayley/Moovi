84ae0cdb0eef5145cea0ed935b2dbc73ba23357e
Karley <karley.waguespack@gmail.com>
2021-10-14 18:46:28 -0500

    added todos for removing movies from the queue

--- lib/likeDislikeBackend.dart
+++ lib/likeDislikeBackend.dart
@@ -6,11 +6,11 @@ var dislikedMovies = <Movie>[];
 
 void onLikeClicked(Movie movie){
   likedMovies.add(movie);
-  //todo remove movie from Chris's queue
+  //todo: remove movie from Chris's queue
 
 }
 
 void onDislikeClicked(Movie movie){
   dislikedMovies.add(movie);
-  //todo add movie to the end of Chris's queue
+  //todo: remove movie and add back to the end of Chris's queue
 }
