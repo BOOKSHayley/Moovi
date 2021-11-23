ddc216489171e4d3a7d9a937e38cf23b37d917e9
BOOKSHayley <hayley.roberts39@ymail.com>
2021-11-23 16:50:02 -0600

    removed words from nav bar

--- lib/main.dart
+++ lib/main.dart
@@ -205,8 +205,8 @@ class _MenusStatefulWidgetState extends State<MenusStatefulWidget> {
       ),
       bottomNavigationBar: BottomNavigationBar(
         backgroundColor: const Color(0xff2a3038),
-        // showSelectedLabels: false,
-        // showUnselectedLabels: false,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
             icon: Icon(
